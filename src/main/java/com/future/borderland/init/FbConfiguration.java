bb66b2479929eef88a1c08cfb82c40763df15999
jiangyunfan <yosku@163.com>
2019-09-08 23:37:32 +0800

    init page

--- src/main/java/com/future/borderland/init/FbConfiguration.java
+++ src/main/java/com/future/borderland/init/FbConfiguration.java
@@ -1,12 +1,19 @@
 package com.future.borderland.init;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created by jiangyunfan on 2019/9/7.
  */
-@Configuration
-public class FbConfiguration {
-
-
-}
+//@Configuration
+//public class FbConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//                .addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/static/");
+//    }
+//}
